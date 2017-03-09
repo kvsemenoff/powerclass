2e226fab6e786710eea58f8f8deea7007be37345
Barasbi <betokov93@gmail.com>
2017-03-09 19:31:19 +0300

    add number #1

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-﻿<?php require_once('includes/header.php'); ?>
-	
-<?php require_once('includes/footer.php'); ?>
+<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header-top.php'); ?>
+<?php require_once('includes/shtraf.php'); ?>
+<?php require_once('includes/how.php'); ?>
+<?php require_once('includes/testimonials.php'); ?>
+<?php require_once('includes/tarif.php'); ?>
+<?php require_once('includes/menu.php'); ?>
+<?php require_once('includes/paralax.php'); ?>
+<?php require_once('includes/want.php'); ?>
+<?php require_once('includes/often.php'); ?>
+<?php require_once('includes/questions.php'); ?>
 
-	
 </body>
 </html>
-
