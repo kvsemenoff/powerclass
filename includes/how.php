4a3115f450da9f0e3cb5b3fbba61f8d5ead88612
engine77 <zaur.1kager@gmail.com>
2017-03-10 14:08:17 +0300

    how

--- includes/how.php
+++ includes/how.php
@@ -1,8 +1,96 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section section_padding dfbghow1">
+	<div class="dfcontainer">
+		<h2 class="dfcaphow">Как это работает</h2>
+		<span class="dfcapbottom">Ежедневное 5-ти разовое питание с бесплатной доставкой на дом</span>
+		
+
+		<div class="dfhowblock">
+			<h1 class="dfhownumber">01</h1>
+			<div class="dfcaptionbox">
+				<h3 class="dfhcap1">Готовим здоровую пищу <br> <span>и упаковываем её в контейнеры</span></h3>
+				<h4 class="dfhcap2">на собственном пищевом производстве</h4>
+				<p class="dfhcap3">Вам больше не нужно тратить время на приготовление пищи для роста мышечной массы. Мы все <b>сделаем за вас.</b> Лучше проведите <b>освободившееся время</b> на <b>тренировках</b> или на <b>отдыхе!</b></p>
+			</div>
+
+			<div class="dfboxchar">
+				<div class="dfimgcahr">
+					<img src="img/howimg1.png" alt="">
+				</div>
+				<div class="dftxtbox">
+					<span class="dfcaptxtbox">Сертифицированное производство</span>
+					<p>Наше пищевое производство поставляет
+					 еду для <b>учащихся школ</b> Москвы и отвечает
+					  всем требованиям <b>СанПиН</b> и <b>ГОСТ</b>.</p>
+				</div>
+			</div>
+			<div class="dfboxchar">
+				<div class="dfimgcahr">
+					<img src="img/howimg2.png" alt="">
+				</div>
+				<div class="dftxtbox">
+					<span class="dfcaptxtbox">Сбалансированное питание по БЖУ</span>
+					<p>Все наборы еды профессионально <b>сбалансированы
+						диетологом</b> по белкам, жирам и углеводам.
+						В приготовлении используются <b>только натуральные</b>
+						продукты без применения консервантов и E-добавок.</p>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
+
+<section class="section dfbghow2">
+	<div class="dfcontainer">		
+		<div class="dfhowblock dfhowblock2">
+		<h2 class="colorwhite"><b>Бесплатно доставляем</b> рационы на дом</h2>
+		<span class="dfcapbottom2">3 раза в неделю – каждые среду, пятницу и воскресенье мы <br>
+		бесперебойно доставляем комплекты питания по Москве и области</span>
+			<h1 class="dfhownumber dfcolor">02</h1>
+			<div class="dfboxchar">
+				<div class="dfimgcahr">
+					<img src="img/howimg3.png" alt="">
+				</div>
+				<div class="dftxtbox dfcolor2">
+					<span class="dfcaptxtbox">Своевременная доставка домой или в офис</span>
+					<p>Заказав питание в PowerClass, вы получите комплект
+						<b>свежеприготовленной еды</b> по выбранному тарифу
+						в любое удобное для вас <b>место и время.</b></p>
+				</div>
+			</div>
+			<div class="dfboxchar">
+				<div class="dfimgcahr">
+					<img src="img/howimg4.png" alt="">
+				</div>
+				<div class="dftxtbox dfcolor2">
+					<span class="dfcaptxtbox">Забудьте про чувство голода в середине дня</span>
+					<p>Комплекты готовой еды бережно упакованы
+						в прочные <b>герметичные упаковки</b>. Вы легко 
+						сможете брать контейнеры с собой <b>на работу
+						или в дорогу</b>. Просто разогрейте и съешьте!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section dfbghow3">
+	<div class="dfcontainer">		
+		<div class="dfhowblock">
+		<h2 class="colorwhite">Вы <b>закрываете вопрос питания</b> <br> и быстрее достигаете своих целей</h2>
+		<span class="dfcapbottom2">С PowerClass вы построите идеальное тело в 2 раза быстрее</span>
+			<h1 class="dfhownumber dfcolor postop">03</h1>
+			<div class="dfboxchar">
+				<div class="dfimgcahr">
+					<img src="img/howimg5.png" alt="">
+				</div>
+				<div class="dftxtbox dfcolor2">
+					<span class="dfcaptxtbox">Больше не  нужно готовить</span>
+					<p>Самостоятельное приготовление сбалансированной
+						пищи занимает более 20 часов в неделю. С готовыми
+						комплексами питания вы сэкономите свое время
+						на отдых и дополнительные тренировки!</p>
+				</div>
+			</div>			
+		</div>
+	</div>
 </section>
