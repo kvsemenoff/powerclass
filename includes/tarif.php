c2b377c832bdc455b24fce57759f0b8775a5c51f
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-03-10 08:52:10 +0600

    added 4 files.php

--- includes/tarif.php
+++ includes/tarif.php
@@ -1,7 +1,19 @@
-<section class="section section_padding">
+<section class="section tarif">
 <div class=" container">
 	<div class="row">
 		<div class="col-md-12">
+			<div class="tarif__head">
+				<h2>Тарифы</h2>
+				<p>Комплексы питания POWERCLASS идеально подходят как мужчинам, так и женщинам</p>
+			</div>
+			<div class="tarif__items clearfix">
+				<div class="col-md-3">
+					<div class="item">
+						
+					</div>
+				</div>
+			</div>
+			<a href="#" class="individual">Рассчитать индивидуальный тариф</a>
 		</div>
 	</div>
 </div>
--- includes/want.php
+++ includes/want.php
@@ -2,7 +2,19 @@
 	<div class=" container">
 		<div class="row">
 			<div class="col-md-12">
-
+				<div class="in">
+					<h2>Не хотите заказывать сразу на неделю?</h2>
+					<span class="descr">Попробуйте «пробный тариф» <b>всего за <i>1199 рублей.</i></b></span>
+					<p>Твой ПП рацион из 5 приемов пищи на 1 день Доставка по Москве бесплатно</p>
+					<div class="form-wrap">
+						<form action="#">
+							<div class="input-wrap">
+								<input class="dd-input" type="text" value="" placeholder="Введите ваш телефон">	
+							</div>	
+							<input class="button-1 dd-submit" type="submit" value="Перезвоните мне" placeholder="">
+						</form>		
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
