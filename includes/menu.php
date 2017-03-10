030969f2c59f0a90a6d6f77c6cd7c8e4cf97298b
engine77 <zaur.1kager@gmail.com>
2017-03-10 19:28:12 +0300

    menu

--- includes/menu.php
+++ includes/menu.php
@@ -2,6 +2,112 @@
 <div class=" container">
 	<div class="row">
 		<div class="col-md-12">
+			<div class="dfmenumobule">
+				<h3 class="dfmenucap">Меню на эту неделю</h3>
+				<div class="dfcallory">
+					<div class="dfcaloryesbox">
+					<span class="dfborder"><a href="#">1 400</a></span>
+						<span>ккал/день</span>
+					</div>
+					<div class="dfcaloryesbox">
+					<span class="dfborder"><a href="#">1 700</a></span>
+						<span>ккал/день</span>
+					</div>
+					<div class="dfcaloryesbox">
+					<span class="dfborder"><a href="#">2 500</a></span>
+						<span>ккал/день</span>
+					</div>
+					<div class="dfcaloryesbox">
+					<span class="dfborder"><a href="#">3 500</a></span>
+						<span>ккал/день</span>
+					</div>
+				</div>
+
+				<div class="df3or5">
+					<a href="#" class="df3">3 ПРИЕМА</a>
+					<a href="#" class="df5">5 ПРИЕМОВ</a>
+					<span>Выберете количество приемов пищи </span>
+				</div>
+
+				<div class="dfweaks">
+					<ul>
+						<li class="dfweak"><a href="#">Пн</a></li>
+						<li class="dfweak"><a href="#">Вт</a></li>
+						<li class="dfweak"><a href="#">Ср</a></li>
+						<li class="dfweak"><a href="#">Чт</a></li>
+						<li class="dfweak"><a href="#">Пт</a></li>
+						<li class="dfweak"><a href="#">Сб</a></li>
+						<li class="dfweak"><a href="#">Вс</a></li>
+					</ul>
+				</div>
+
+				<div class="dfblockmenu">
+					<div class="dfmenubox1">
+						<div class="dfimgmenu"><img src="img/menumob1.jpg" alt=""></div>
+						<div class="dfmenutxt">
+							<h4 class="dfnameproduct">Гречневая каша на молоке</h4>
+							<span class="dfcharmenu1">Белки: 40 г. Жиры: 18 г. Углеводы: 59 г.</span><br>
+							<span class="dfcharmenu2">564 ккал</span>
+							<span class="dfcharmenu3">1</span>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<div class="dfmenubox1">
+						<div class="dfimgmenu"><img src="img/menumob2.jpg" alt=""></div>
+						<div class="dfmenutxt">
+							<h4 class="dfnameproduct">Бананово-творожный пирог/фрукты сезонные</h4>
+							<span class="dfcharmenu1">Белки: 40 г. Жиры: 18 г. Углеводы: 59 г.</span><br>
+							<span class="dfcharmenu2">564 ккал</span>
+							<span class="dfcharmenu3">2</span>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<div class="dfmenubox1">
+						<div class="dfimgmenu"><img src="img/menumob1.jpg" alt=""></div>
+						<div class="dfmenutxt">
+							<h4 class="dfnameproduct">Гречневая каша на молоке</h4>
+							<span class="dfcharmenu1">Белки: 40 г. Жиры: 18 г. Углеводы: 59 г.</span><br>
+							<span class="dfcharmenu2">564 ккал</span>
+							<span class="dfcharmenu3">3</span>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<div class="dfmenubox1">
+						<div class="dfimgmenu"><img src="img/menumob3.jpg" alt=""></div>
+						<div class="dfmenutxt">
+							<h4 class="dfnameproduct">Индейка гриль / перловка с овощами /соус тар тар</h4>
+							<span class="dfcharmenu1">Белки: 40 г. Жиры: 18 г. Углеводы: 59 г.</span><br>
+							<span class="dfcharmenu2">564 ккал</span>
+							<span class="dfcharmenu3">4</span>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<div class="dfmenubox1">
+						<div class="dfimgmenu"><img src="img/menumob1.jpg" alt=""></div>
+						<div class="dfmenutxt">
+							<h4 class="dfnameproduct">Гречневая каша на молоке</h4>
+							<span class="dfcharmenu1">Белки: 40 г. Жиры: 18 г. Углеводы: 59 г.</span><br>
+							<span class="dfcharmenu2">564 ккал</span>
+							<span class="dfcharmenu3">5</span>
+						</div>
+						<div class="clear"></div>
+					</div>
+
+
+				</div>
+
+				<div class="dfitogo">
+					<span class="dftxtitogo1"><b>Итого:</b></span>
+					<span class="dftxtitogo2"><b>3 495 </b>Ккал</span> <br>
+					<span class="dftxtitogo2"><b>245 </b>Белки</span>
+					<span class="dftxtitogo2"><b>96 </b>Жиры</span>
+					<span class="dftxtitogo2"><b>411 </b>Углеводы</span>
+				</div>
+
+				<div class="dfmenulink">
+					<a href="#">хочу попробовать</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
