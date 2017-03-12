0defdf1c63365b9ea942e63618c007340581f485
Berzeg07 <berezg86@gmail.com>
2017-03-12 14:00:27 +0300

    29.14

--- popup.php
+++ popup.php
@@ -1,11 +1,288 @@
 ﻿<?php require_once('includes/header.php'); ?>
+<br><br>
+<a href="#log-in-1" name="modal">Оставьте заявку</a><br><br>	
+<a href="#log-in-2" name="modal">Заказать комплект питания</a><br><br>
+<a href="#log-in-3" name="modal">Заказать комплект питания 2</a><br><br>
+<a href="#log-in-4" name="modal">5 раз в день</a><br><br>
 
-<a href="#">Оставьте заявку</a><br><br>	
+
+<div id="mask"></div>
+
+<!-- Pop-1 -->
+<!-- /////////////////////////// -->
+<div id="log-in-1" class="window">
+	<div class="dd-form-wrap">
+
+		<form class="form1" action="#" method="post">
+
+			<div class="form-wrap-main">
+
+				<div class="dd-close"></div>
+
+				<div class="dd-title-1">
+					<span>Оставьте заявку и менеджер PowerClass</span><br>
+					<span>свяжется с вами в течение 30 минут</span>
+				</div>
+
+				<div class="dd-el_wrap">	
+
+					<div class="dd-el-1 dd-el-1_txt">
+
+						<div class="dd-img-wrap-1">
+
+							<div class="dd-img fl-left">
+								<img src="img/bel.png" alt="">
+							</div>
+
+							<div class="dd-img-txt-1 fl-left">
+								<span class="dd-bold-title_pop">Белла</span><br>
+								<span class="dd-small-title_pop">Эксперт по здоровому </span><br>
+								<span class="dd-small-title_pop">питанию PowerClass</span>
+							</div>
+							<div class="clearfix"></div>
+
+						</div>
+
+						<div class="dd-main-txt-1">
+							<p class="dd-small-txt_pop">
+								Всего за 2 минуты телефонного разговора я помогу вам определиться с тарифом на основе ваших целей, потребностей и возможностей, а также отвечу на любые вопросы.
+							</p>
+						</div>
+
+					</div>
+
+					<div class="dd-el-1 dd-el-1_inp">
+						<input class="dd-input dd-input_pop" type="text" name="uname" placeholder="Введите ваше имя">
+						<input class="dd-input dd-input_pop phone" name="phone" type="text" placeholder="Введите ваш телефон" >
+						<input class="button-1 dd-submit" type="submit" name="submit" value="перезвоните мне">
+					</div>
+					<div class="clearfix"></div>
+
+				</div>
+
+			</div>
+
+		</form>
+
+	</div>
+</div>
+
+<!-- Pop-2 -->
+<!-- /////////////////////////// -->
+<div id="log-in-2" class="window">
+
+	<div class="dd-form-wrap">
+
+		<form class="form1 dd-bg" action="#" method="post">
+
+			<div class="dd-img-2">
+				<img src="img/bag.png" alt="">	
+			</div>
+
+			<div class="form-wrap-main form-wrap-main-2">
+
+				<div class="dd-close"></div>
+
+				<div class="dd-el-1 dd-el-1_inp pr28">
+					<div class="dd-title-2_wrap">
+						<span class="dd-title-2">
+							Заказать комплект питания
+						</span>
+						<span class="dd-bold-title_pop">Спорт плюс, <span>3 500 ккал/день</span></span><br>
+						<span class="dd-grey-txt">5-разовое питание, на 5 дней</span><br>
+						<span class="dd-main-txt-2">Сумма заказа: <span>7 400 руб.</span></span>
+					</div>
+					<input class="dd-input dd-input_pop" type="text" name="uname" placeholder="Введите ваше имя">
+					<input class="dd-input dd-input_pop phone" name="phone" type="text" placeholder="Введите ваш телефон" >
+					<input class="button-1 dd-submit" type="submit" name="submit" value="оформить по телефону">
+				</div>
+				<div class="dd-but_online">
+					<a class="button-2" href="#">Оформить онлайн</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="clearfix"></div>
+
+		</form>
+
+	</div>
+</div>
+
+<!-- Pop-3 -->
+<!-- /////////////////////////// -->
+<div id="log-in-3" class="window">
+
+	<div class="dd-form-wrap">
+
+		<form class="form1 dd-bg" action="#" method="post">
+
+			<div class="dd-img-2">
+				<img src="img/bag.png" alt="">	
+			</div>
+
+			<div class="form-wrap-main form-wrap-main-2">
+
+				<div class="dd-close"></div>
+
+				<div class="dd-el-1 dd-el-1_inp pr28">
+
+					<div class="dd-title-2_wrap">
+						<span class="dd-title-2">
+							Заказать комплект питания
+						</span>
+						<span class="dd-bold-title_pop">Спорт плюс, <span>3 500 ккал/день</span></span><br>
+					</div>
+
+					<label class="dd-label">
+						<input class="checkbox" type="radio" name="checkbox-test">
+						<span class="checkbox-custom"></span>
+						<span class="dd-txt-radio">Пробный день</span><br>
+						<span class="dd-grey-txt dd-grey-txt_2">(Четверг)</span>
+						
+						<span class="label"></span>
+					</label>
+					<label class="dd-label mb25">
+						<input class="checkbox" type="radio" name="checkbox-test" checked>
+						<span class="checkbox-custom"></span>
+						<span class="dd-txt-radio activeRed">На неделю</span>
+						<span class="label"></span>
+					</label>
+					<div class="clearfix"></div>
+					<input class="dd-input dd-input_pop" type="text" name="uname" placeholder="Введите ваше имя">
+					<input class="dd-input dd-input_pop phone" name="phone" type="text" placeholder="Введите ваш телефон" >
+					<input class="button-1 dd-submit" type="submit" name="submit" value="Заказать питание">
+
+				</div>
+
+				<div class="clearfix"></div>
+			</div>
+			<div class="clearfix"></div>
+
+		</form>
+
+	</div>
+</div>
+
+<!-- Pop-4 -->
+<!-- /////////////////////////// -->
+<div id="log-in-4" class="window">
+
+	<div class="dd-form-wrap">
+
+		<form class="form1" action="#" method="post">
+			<div class="form-wrap-main form-wrap-main_bg">
+
+				<div class="dd-close"></div>
+
+				<div class="dd-el-1 dd-el-1_inp pr28">
+
+					<div class="dd-title-2_wrap">
+						<span class="dd-title-2">
+							Начните питаться 
+							сбалансированной пищей 
+							5 раз в день, и ваше тело 
+							скажет вам «Спасибо» 
+						</span>
+					</div>
+					<input class="dd-input dd-input_pop" type="text" name="uname" placeholder="Введите ваше имя">
+					<input class="dd-input dd-input_pop phone" name="phone" type="text" placeholder="Введите ваш телефон" >
+					<input class="button-1 dd-submit" type="submit" name="submit" value="Заказать питание">
+
+				</div>
+
+				<div class="clearfix"></div>
+			</div>
+
+		</form>
+
+	</div>
+</div>
 
 
+<div id="thanks" class="window">
+	<div class="form-wrap-main">
+		<div class="dd-close"></div>
+		<div class="form-title">
+			Спасибо за заявку!
+		</div>
+		<div class="dd-form-txt">
+			Наш менеджер свяжется с вами в ближайшее время!
+		</div>
+	</div>
+</div>
+<a href="#thanks" name="modal"></a>
 
+<script>
+	$('.checkbox').click(function(){
+		$('.dd-txt-radio').toggleClass('activeRed');
+	});
+</script>
 
-	
+<script>
+
+	$(document).ready(function(){
+
+		$('a[name=modal]').click(function(e) {
+			e.preventDefault();
+			var id = $(this).attr('href');
+			var maskHeight = $(document).height();
+			var maskWidth = $(window).width();
+			$('#mask').css({'width':maskWidth,'height':maskHeight});
+			$('#mask').fadeTo("slow",0.8); 
+			var winH = $(window).height();
+			var winW = $(window).width();
+			posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement ||document.body.parentNode || document.body).scrollTop;
+			$(id).css('top',  posTop+50);
+			$(id).css('left', winW/2-$(id).width()/2);
+			$(id).fadeIn(500); 
+		});
+		$('.window .dd-close').click(function (e) {
+			e.preventDefault();
+			$('#mask, .window').hide();
+			$('.window').hide();
+		}); 
+
+		$('#mask, .an-exit__krest').click(function () {
+			$('#mask').hide();
+			$('.window').hide();
+		});
+
+		$(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
+
+		$(".form1").submit(function() { 
+			var tel = $(this).find('input[name="phone"]');
+			var empty = false;
+			if (tel.val() == ""){
+				empty = true;
+			}
+			if (empty == true){
+				tel.addClass("error-input");
+				tel.focus();
+			}else{
+				var form_data = $(this).serialize(); 
+				$.ajax({
+					type: "POST", 
+					url: "/sendmessage.php", 
+					data: form_data,
+					success: function() {
+						cleanTnanks(this);
+					}
+				});
+			}
+			return false;
+		});
+
+		function cleanTnanks(form){
+			$('input[type="text"]').removeClass("error-input");
+			$("input[type=text], textarea").val("");
+			$('.window').hide();
+			$('a[href=#thanks]').trigger('click');
+				// location = "spasibo.php";
+			};
+		});
+
+	</script>
+
 </body>
 </html>
 
