a4f7cc5e07322ee57c3531f4a2e5e4521acd7902
engine77 <zaur.1kager@gmail.com>
2017-03-14 18:25:43 +0300

    demo

--- shtraf-demo.php
+++ shtraf-demo.php
@@ -1,7 +1,7 @@
 ﻿<?php require_once('includes/header.php'); ?>
 
 <section class="section shtraf">
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif1">
 		<div class="top-line clearfix">
 			<div class="nav active"><span>шаг 1</span></div>
 			<div class="nav"><span>шаг 2</span></div>
@@ -19,8 +19,8 @@
 						<p>Стоимость питания будет рассчитана в соответствии с вашей потребностью в калориях и расписанием на неделю</p>
 						<span class="descr">Выберите ваш пол</span>
 						<div class="btn-wrap">
-							<button class="shtraf__btn shtraf__btn--black"><span>Мужской</span></button>
-							<button class="shtraf__btn shtraf__btn--red">Женский</button>
+							<button class="shtraf__btn shtraf__btn--black" pol="5"><span>Мужской</span></button>
+							<button class="shtraf__btn shtraf__btn--red" pol="161">Женский</button>
 						</div>
 						<a href="#" class="shtraf__link">Полный прайс-лист</a>
 					</div>
@@ -28,7 +28,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif2">
 		<div class="top-line clearfix">
 			<div class="nav active"><a href="#">< <i>назад к шагу 1</i></a></div>
 			<div class="nav active"><span>шаг 2</span></div>
@@ -56,7 +56,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif3">
 		<div class="top-line clearfix">
 			<div class="nav active"><a href="#">шаг 1</a></div>
 			<div class="nav active"><a href="#">< <i>назад к шагу 2</i></a></div>
@@ -76,15 +76,15 @@
 						<div class="btn-wrap">
 							<div class="input-wrap">
 								<span>Возраст:</span>
-								<input type="number" value="26" class="input-number" style="width:88px">
+								<input type="number" value="25"  min="12" max="99" class="input-number" id="jsage" style="width:88px">
 							</div>
 							<div class="input-wrap">
 								<span>Рост:</span>
-								<input type="number" value="176" class="input-number" style="width:88px">
+								<input type="number" value="175" min="50" max="300" class="input-number" style="width:88px" id="jsrost">
 							</div>
 							<div class="input-wrap">
 								<span>Вес:</span>
-								<input type="number" value="76" class="input-number" style="width:88px">
+								<input type="number" value="75" min="30" max="300" class="input-number" style="width:88px" id="jsmass">
 							</div>
 							<button class="shtraf__btn shtraf__btn--black shtraf__btn--next">Далее</button>
 						</div>
@@ -94,7 +94,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif4">
 		<div class="top-line clearfix">
 			<div class="nav active"><a href="#">шаг 1</a></div>
 			<div class="nav active"><a href="#">шаг 2</a></div>
@@ -112,11 +112,11 @@
 						<p>Стоимость питания будет рассчитана в соответствии с вашей потребностью в калориях и расписанием питания</p>
 						<span class="descr">Выберите вашу цель</span>
 						<div class="btn-wrap">
-							<button class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Не занимаюсь спортом</span><i>сидячий образ</i></button>
-							<button class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Легкая активность</span><i>аэробные нагрузки 1-3 р/нед</i></button>
-							<button class="shtraf__btn shtraf__btn--red shtraf__btn--big">Повышенная активность<i>интенсивные тренировки 3-5 р/нед</i></button>
-							<button class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Высокая активность</span><i>тяжелые тренировки 6-7 р/нед</i></button>
-							<button class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Экстремальная активность</span><i>тяжелые тренировки 2 раза в день</i></button>
+							<button activity="1.2" class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Не занимаюсь спортом</span><i>сидячий образ</i></button>
+							<button activity="1.375" class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Легкая активность</span><i>аэробные нагрузки 1-3 р/нед</i></button>
+							<button activity="1.55" class="shtraf__btn shtraf__btn--red shtraf__btn--big">Повышенная активность<i>интенсивные тренировки 3-5 р/нед</i></button>
+							<button activity="1.725" class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Высокая активность</span><i>тяжелые тренировки 6-7 р/нед</i></button>
+							<button activity="1.9" class="shtraf__btn shtraf__btn--black shtraf__btn--big"><span>Экстремальная активность</span><i>тяжелые тренировки 2 раза в день</i></button>
 						</div>
 						<a href="#" class="shtraf__link">Полный прайс-лист</a>
 					</div>
@@ -124,7 +124,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif5">
 		<div class="top-line clearfix">
 			<div class="nav active"><a href="#">шаг 1</a></div>
 			<div class="nav active"><a href="#">шаг 2</a></div>
@@ -143,11 +143,11 @@
 						<span class="descr">Здоровое питание состоит из 5 приемов пищи в день. <br>Выберите свой распорядок.</span>
 						<div class="btn-wrap">
 							<div class="half">
-								<button class="shtraf__btn shtraf__btn--red shtraf__btn--eat">Готовлю завтрак и ужин самостоятельно<i>рассчитайте мне <b>3 приема</b> пищи, чтобы брать с собой</i></button>
+								<button id="jsfood3"  class="shtraf__btn shtraf__btn--red  shtraf__btn--eat" myfood="3">Готовлю завтрак и ужин самостоятельно<i>рассчитайте мне <b>3 приема</b> пищи, чтобы брать с собой</i></button>
 								<div class="btn-dots"><i class="empty"></i><i></i><i></i><i></i><i class="empty"></i></div>
 							</div>
 							<div class="half">
-								<button class="shtraf__btn shtraf__btn--black shtraf__btn--eat"><span>Не хочу готовить и отвлекаться</span><i>полноценное <b>5-разовое</b> питание на весь день</i></button>
+								<button id="jsfood5" class="shtraf__btn shtraf__btn--black shtraf__btn--eat" myfood="5" ><span>Не хочу готовить и отвлекаться</span><i>полноценное <b>5-разовое</b> питание на весь день</i></button>
 								<div class="btn-dots"><i></i><i></i><i></i><i></i><i></i></div>
 							</div>
 						</div>
@@ -157,7 +157,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif6">
 		<div class="top-line clearfix">
 			<div class="nav active"><a href="#">шаг 1</a></div>
 			<div class="nav active"><a href="#">шаг 2</a></div>
@@ -176,11 +176,11 @@
 						<span class="descr">Понадобится ли вам готовое питание <br> на выходных (Сб, Вс)?</span>
 						<div class="btn-wrap btn-wrap-mini">
 							<div class="half">
-								<button class="shtraf__btn shtraf__btn--black shtraf__btn--next"><span>Да</span></button>
+								<button class="shtraf__btn shtraf__btn--black shtraf__btn--next" id="jsdays7" days="7"><span>Да</span></button>
 								<div class="btn-dots"><i></i><i></i><i></i><i></i><i></i></div>
 							</div>
 							<div class="half">
-								<button class="shtraf__btn shtraf__btn--black shtraf__btn--next"><span>Нет</span></button>
+								<button id="jsdays5" days="5" class="shtraf__btn shtraf__btn--black shtraf__btn--next"><span>Нет</span></button>
 								<div class="btn-dots"><i></i><i></i><i></i><i class="empty"></i><i class="empty"></i></div>
 							</div>
 						</div>
@@ -190,7 +190,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif7">
 		<div class="top-line clearfix">
 			<div class="nav active"><a href="#">шаг 1</a></div>
 			<div class="nav active"><a href="#">шаг 2</a></div>
@@ -209,15 +209,15 @@
 						<div class="end clearfix">
 							<div class="col-md-offset-1 col-md-2 end__item">
 								<span class="end__title">Ваша дневная <br>норма калорий:</span>
-								<span class="kkal">3472</span>
+								<span class="kkal jsitogo">3472</span>
 								<span class="end__title">ккал/день</span>
 							</div>
 							<div class="col-md-3 end__item">
 								<span class="end__title">Рекомендуемый тариф:</span>
 								<span class="end__tarif">Спорт Плюс</span>
-								<span class="end__title">3500 ккал/день</span>
+								<span class="end__title jskkalday"><b>3500</b> ккал/день</span>
 								<div class="end__info">
-									<span>5-разовое питание, 7 дней</span>
+									<span class="jsrazdney"><b>5</b>-разовое питание, <em>7</em> дней</span>
 									<i>Дни доставки: Пн, Ср, Пт</i>
 								</div>
 								<div class="end__price">
@@ -230,7 +230,7 @@
 								<div class="form-wrap">
 									<form action="#">
 										<div class="input-block">
-											<input class="dd-input" type="text" value="" placeholder="Введите ваше имя">	
+											<input class="dd-input" type="text" value="" placeholder="Введите ваше имя" required>	
 										</div>
 										<div class="input-block">
 											<input class="dd-input" type="text" value="" placeholder="Введите ваш телефон">		
@@ -251,7 +251,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-item">
+	<div class="shtraf-item" id="jstarif8">
 		<div class="top-line clearfix">
 			<div class="nav active"><a href="#">шаг 1</a></div>
 			<div class="nav active"><a href="#">шаг 2</a></div>
@@ -336,20 +336,24 @@
 			</div>
 		</div>
 	</div>
-	<div class="shtraf-mini">
+
+
+<!-- =====================mobileversion========================= -->
+
+	<div class="shtraf-mini" id="jsmobversion">
 		<div class="inner">
 					<h2>Рассчитайте ваш персональный тариф</h2>
 					<div class="form-wrap">
 						<form action="#">
 							<div class="select-wrap border-red">
-								<select>
+								<select class="jsselected1">
 									<option disabled>Выберите ваш пол</option>
-									<option>Мужской</option>
-									<option>Женский</option>
+									<option polmob="5">Мужской</option>
+									<option polmob="161">Женский</option>
 								</select>
 							</div>
 							<div class="select-wrap">
-								<select>
+								<select >
 									<option disabled>Выберите вашу цель</option>
 									<option>Сбросить вес</option>
 									<option>Поддержать форму</option>
@@ -357,35 +361,36 @@
 								</select>
 							</div>
 							<div class="select-wrap clearfix">
-								<input type="number" class="input-mini" placeholder="Возраст">
-								<input type="number" class="input-mini border-red" placeholder="Рост">
-								<input type="number" class="input-mini" placeholder="Вес">
+								<input type="number" value="25" min="12" max="99" class="input-mini" placeholder="Возраст" id="agemob">
+								<input type="number" value="175" min="50" max="300" class="input-mini border-red" placeholder="Рост" id="rostmob">
+								<input type="number" value="75" min="30" max="300" class="input-mini" placeholder="Вес" id="massmob">
 							</div>
 							<div class="select-wrap">
-								<select>
+								<select class="jsselected2">
 									<option disabled>Уровень активности</option>
-									<option>Не занимаюсь спортом</option>
-									<option>Легкая активность</option>
-									<option>Повышенная активность</option>
-									<option>Высокая оактивность</option>
-									<option>Экстремальная активность</option>
+									<option activitymob="1.2">Не занимаюсь спортом</option>
+									<option activitymob="1.375">Легкая активность</option>
+									<option activitymob="1.55">Повышенная активность</option>
+									<option activitymob="1.725">Высокая оактивность</option>
+									<option activitymob="1.9">Экстремальная активность</option>
 								</select>
+								
 							</div>
 							<div class="select-wrap">
-								<select>
+								<select class="jsselected3">
 									<option disabled>Выберите свой распорядок дня</option>
-									<option>Готовлю самостоятельно</option>
-									<option>Не хочу готовить</option>
+									<option myfoodmob="3">Готовлю самостоятельно</option>
+									<option myfoodmob="5">Не хочу готовить</option>
 								</select>
 							</div>
 							<div class="select-wrap">
-								<select>
+								<select class="jsselected4">
 									<option disabled>Питание на выходных (Сб, Вс)?</option>
-									<option>Да</option>
-									<option>Нет</option>
+									<option daysmob="7">Да</option>
+									<option daysmob="5">Нет</option>
 								</select>
 							</div>
-							<input class="button-1 dd-submit" type="submit" value="Рассчитать тариф">
+							<input class="button-1 dd-submit" id="jsrasschitat" type="submit" value="Рассчитать тариф">
 							<a href="#" class="link">Полный прайс-лист</a>
 						</form>		
 					</div>
@@ -395,4 +400,213 @@
 	
 </body>
 </html>
+
+<script>
+ var male="",
+	cel="", 
+	age="", 
+	rost="", 
+	mass="", 
+	activity="", 
+	food="", 
+	days="";
+
+	$('.shtraf__btn').on('click', function(e){
+
+		if ($('#jstarif1').css('display')==='block') {
+			male += $(this).attr('pol');
+		}
+		else if($('#jstarif3').css('display')==='block'){
+
+			if ($('#jsage').val() >= 99 || $('#jsage').val() <= 12) {
+				$('#jsage').addClass('disabledinputjs');
+				$(this).addClass('disabledjs');	
+			}else if($('#jsrost').val() >= 300 || $('#jsrost').val() <= 50){
+				$('#jsrost').addClass('disabledinputjs');
+				$(this).addClass('disabledjs');		
+			}else if($('#jsmass').val() >= 300 || $('#jsmass').val() <= 30){
+				$('#jsmass').addClass('disabledinputjs');
+				$(this).addClass('disabledjs');		
+			}else{
+				$(this).removeClass('disabledjs');
+				age += $(this).parents('.btn-wrap').find('#jsage').val();
+				rost += $(this).parents('.btn-wrap').find('#jsrost').val();
+				mass += $(this).parents('.btn-wrap').find('#jsmass').val();
+			}
+
+			
+		}
+		else if($('#jstarif4').css('display')==='block'){
+			activity +=	$(this).attr('activity');
+		}
+		else if($('#jstarif5').css('display')==='block'){
+			food += $(this).attr('myfood');
+			$('.jsrazdney b').text(food);
+		}
+		else if($('#jstarif6').css('display')==='block'){
+			days += $(this).attr('days');
+			$('.jsrazdney em').text(days);
+		}
+		
+		if (!$('.shtraf__btn').hasClass('disabledjs')) {
+			$(this).parents('.shtraf-item').hide().next().show();
+		}
+	
+
+		if ($('#jstarif7').css('display')==='block') {
+
+			if (male==5) {
+				var itogo = (10 * +mass + 6.25 * +rost - 5 * +age + 5) * +activity;
+				$('.jsitogo').text(Math.round(itogo));
+
+				if (itogo <= 1400) {
+					$('.end__tarif').text('Фит');
+					$('.jskkalday b').text('1400');
+				}
+				else if(itogo > 1400 && itogo <= 1700){
+					$('.end__tarif').text('Фит плюс');
+					$('.jskkalday b').text('1700');	
+				}
+				else if(itogo > 1700 && itogo <= 2500){
+					$('.end__tarif').text('Спорт');	
+					$('.jskkalday b').text('2500');
+				}
+				else if(itogo > 2500 && itogo <= 3500){
+					$('.end__tarif').text('Спорт плюс');
+					$('.jskkalday b').text('3500');	
+				}
+
+
+			}else {
+				var itogo = (10 * +mass + 6.25 * +rost - 5 * +age - 161) * +activity;
+				$('.jsitogo').text(Math.round(itogo));
+
+				if (itogo <= 1400) {
+					$('.end__tarif').text('Фит');
+					$('.jskkalday b').text('1400');
+				}
+				else if(itogo > 1400 && itogo <= 1700){
+					$('.end__tarif').text('Фит плюс');
+					$('.jskkalday b').text('1700');	
+				}
+				else if(itogo > 1700 && itogo <= 2500){
+					$('.end__tarif').text('Спорт');	
+					$('.jskkalday b').text('2500');
+				}
+				else if(itogo > 2500 && itogo <= 3500){
+					$('.end__tarif').text('Спорт плюс');
+					$('.jskkalday b').text('3500');	
+				}
+			}
+		}
+
+	});
+	$('.nav a i').on("click", function(){
+		$(this).parents('.shtraf-item').hide().prev().show();
+	});
+</script>
+
+
+
+<script>
+	//============================mobversion
+
+	$('#jsrasschitat').on('click', function(event) {
+		var malemob="",
+		celmob="", 
+		agemob="", 
+		rostmob="", 
+		massmob="", 
+		activitymob="", 
+		foodmob="", 
+		daysmob="";
+
+		event.preventDefault();
+
+		malemob +=  $(this).prevAll('.select-wrap')
+				.find('.jsselected1')
+				.find('option:selected')
+				.attr('polmob');
+
+		agemob +=  $(this).prevAll('.select-wrap')
+				.find('#agemob')				
+				.val();	
+
+		rostmob +=  $(this).prevAll('.select-wrap')
+				.find('#rostmob')				
+				.val();	
+
+		massmob +=  $(this).prevAll('.select-wrap')
+				.find('#massmob')				
+				.val();			
+
+		activitymob +=  $(this).prevAll('.select-wrap')
+				.find('.jsselected2')
+				.find('option:selected')
+				.attr('activitymob');
+				
+		foodmob +=  $(this).prevAll('.select-wrap')
+				.find('.jsselected3')
+				.find('option:selected')
+				.attr('myfoodmob');
+				
+		daysmob +=  $(this).prevAll('.select-wrap')
+				.find('.jsselected4')
+				.find('option:selected')
+				.attr('daysmob');
+
+
+		if (malemob==5) {
+		var itogomob = (10 * +massmob + 6.25 * +rostmob - 5 * +agemob + 5) * +activitymob;
+
+			// $('.jsitogo').text(Math.round(itogo));
+
+			// if (itogo <= 1400) {
+			// 	$('.end__tarif').text('Фит');
+			// 	$('.jskkalday b').text('1400');
+			// }
+			// else if(itogo > 1400 && itogo <= 1700){
+			// 	$('.end__tarif').text('Фит плюс');
+			// 	$('.jskkalday b').text('1700');	
+			// }
+			// else if(itogo > 1700 && itogo <= 2500){
+			// 	$('.end__tarif').text('Спорт');	
+			// 	$('.jskkalday b').text('2500');
+			// }
+			// else if(itogo > 2500 && itogo <= 3500){
+			// 	$('.end__tarif').text('Спорт плюс');
+			// 	$('.jskkalday b').text('3500');	
+			// }
+
+		}
+
+		else {		
+	var itogomob = (10 * +massmob + 6.25 * +rostmob - 5 * +agemob - 161) * +activitymob;	
+			// $('.jsitogo').text(Math.round(itogo));
+
+		// if (itogo <= 1400) {
+		// 	$('.end__tarif').text('Фит');
+		// 	$('.jskkalday b').text('1400');
+		// }
+		// else if(itogo > 1400 && itogo <= 1700){
+		// 	$('.end__tarif').text('Фит плюс');
+		// 	$('.jskkalday b').text('1700');	
+		// }
+		// else if(itogo > 1700 && itogo <= 2500){
+		// 	$('.end__tarif').text('Спорт');	
+		// 	$('.jskkalday b').text('2500');
+		// }
+		// else if(itogo > 2500 && itogo <= 3500){
+		// 	$('.end__tarif').text('Спорт плюс');
+		// 	$('.jskkalday b').text('3500');	
+		// }
+	}
+	console.log(itogomob);
+// console.log(malemob, agemob, rostmob, massmob, activitymob, foodmob, daysmob);							
+
+	});
+
+
+	
+</script>
 
